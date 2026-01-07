80a0c6d96fbc4e645f8aee48102292c74bbf6f04
Zuraiz Ahmad <zuraizazahmad990@gmail.com>
2026-01-07 19:25:58 +0500

    Revise header comments in NVHGzipFile.swift
    Updated file header comments for NVHGzipFile.swift.

--- Sources/NVHTarGzip_Swift/NVHGzipFile.swift
+++ Sources/NVHTarGzip_Swift/NVHGzipFile.swift
@@ -1,8 +1,12 @@
 //
-//  NVHGzipFileErrorType.swift
-//  Quran Chat
+//  NVHGzipFile.swift
+//  NVHTarGzip-Swift
 //
-//  Created by Macbook Pro on 07/01/2026.
+//  Created by roxx990 on 07/01/2026.
+//  Copyright © 2026 roxx990. All rights reserved.
+//
+//  A pure-Swift reimplementation of NVHTarGzip, supporting .tar, .gz, and .tar.gz archives
+//  with progress tracking on Apple platforms.
 //
 
 
